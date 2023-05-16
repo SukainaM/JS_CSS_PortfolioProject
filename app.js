b8693219dbd91aa3090f53e62286ed276980e3cd
sm-360 <sm360@hw.ac.uk>
2023-05-16 04:15:52 +0400

    modal window for projects

--- app.js
+++ app.js
@@ -10,4 +10,17 @@
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
     })
-})();
+})()
+
+function onClick(element) {
+    document.getElementById("image").src = element.src;
+    document.getElementById("cardModal").style.display = "block";
+    // OnClick="Add_Text();
+  };
+
+  function Add_Text(){
+    var Elm = ocument.getElementById("Span_1");
+    var TheText = "portfolio desc test";
+    Elm.innerHTML = TheText;
+  }
+  
